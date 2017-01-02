6f63787608492d1b4141e3a473b328542be25904
Maik <mail@web-rider.de>
2017-01-02 20:03:41 +0800

    Rewriting Messages output

--- templates/privatemsg-view.tpl.php
+++ templates/privatemsg-view.tpl.php
@@ -15,7 +15,8 @@
     <?php if (isset($new)): ?>
       <span class="new privatemsg-message-new"><?php print $new ?></span>
     <?php endif ?>
-      <div class="privatemsg-message-information">
+      
+    <div class="privatemsg-message-information">
         <span class="privatemsg-message-date pull-right"><?php print format_date($message -> timestamp, 'short'); ?></span>
         Von <?php print \LK\u($message -> author -> uid); ?>
       </div>
@@ -24,45 +25,26 @@
       <div class="panel-body">  
     <div class="pull-left">
       <?php print $message_body; ?>
-      
     </div>
     
      <div class="pull-right"><?php if (isset($message_actions)): ?>
          <a class="btn btn-danger btn-sm" href="<?php print url('messages/delete/'. $message -> thread_id .'/'. $message -> mid); ?>">Löschen</a>
-       <?php endif ?></div>
-    
+       <?php endif ?></div>    
   </div>
 </div>
 </div></div>
 
 <?php
-  if(isset($message->field_search_query['und'][0]['value'])){
-      $search = unserialize($message->field_search_query['und'][0]['value']);
-      
-      if($search AND !defined("LK_LOCAL")){
-          print lk_theme_search_result_block($search);  
-          
-          $result = lk_theme_search_result_view($search);  
-      
-      }
-  }
-  elseif(isset($message->field_msg_kampagnen["und"])){
-     foreach($message->field_msg_kampagnen["und"] as $nodeobj){
-        if($nodeobj["access"]){
-            $node = node_load($nodeobj["nid"]);
-            $view = node_view($node, 'teaser');
-            print render($view);
-        }
-     }
-  }
-  elseif(isset($message->field_neuigkeit['und'][0]['target_id'])){
-      $entities = entity_load('neuigkeit', array($message->field_neuigkeit['und'][0]['target_id']));
-      $view = entity_view('neuigkeit', $entities, 'teaser');
-      print render($view);
+
+  if(isset($search)){
+      print render($search);
   }
   
+  if(isset($news)){
+      print render($news);
+  }
+
+  if(isset($kampagnen)){
+      print render($kampagnen);
+  }
   
-               
-?>
-
-
